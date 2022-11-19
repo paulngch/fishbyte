96361d66ef8f7fd49a560ad85eb4393742babe3c
Your Name <you@example.com>
2022-11-19 13:14:01 +0800

    api test Datagovsg 2hour weather forecast

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
+import ApiTest from './components/ApiTest'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -23,6 +24,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+        <ApiTest/ >
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
---
+++ src/components/ApiTest.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default function ApiTest () {
+    const queryUrl="https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?date_time=2022-11-19T23%3A09%3A00";
+    
+    console.log(queryUrl);
+
+return(
+    <div>
+
+    </div>
+)
+}
