24ea5f26856d2fe60632275212853b525bce4797
Your Name <you@example.com>
2022-11-19 18:41:40 +0800

    fixing error

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import ApiTest from './components/ApiTest'
+import { useState, useEffect } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+import ApiTest from "./components/ApiTest";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
@@ -24,13 +24,13 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
-        <ApiTest/ >
+        <ApiTest />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/ApiTest.jsx
+++ src/components/ApiTest.jsx
@@ -1,13 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useEffect, useState } from "react";
 
-export default function ApiTest () {
-    const queryUrl="https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?date_time=2022-11-19T23%3A09%3A00";
-    
-    console.log(queryUrl);
+export default function ApiTest() {
+  const [wForecast2, setWForecast2] = useState({});
 
-return(
+  const queryUrl =
+    "https://api.data.gov.sg/v1/environment/2-hour-weather-forecast?date_time=2022-11-19T23%3A09%3A00";
+
+  //   console.log(queryUrl);
+
+
+  useEffect(() => {
+    fetch(queryUrl)
+      .then((response) => response.json())
+      .then((data) => setWForecast2(data));
+  }, []);
+
+
+    const temp = wForecast2;
+    // console.log(temp)
+    console.log(temp.api_info.status)
+
+  return (
     <div>
-
+        test
     </div>
-)
+  );
 }
